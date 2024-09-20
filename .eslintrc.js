b4f775f35828e274ae49116b115158f63be1d6f1
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-20 14:09:02 +0200

    Solved all exercises.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jasmine: true
-  },
-  extends: ['standard', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 12
-  },
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error']
-  }
-}
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
